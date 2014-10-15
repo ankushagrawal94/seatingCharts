8ee4add07e240a9128801a6fc60c67450972d968
Ankush Agrawal <a3agrawa@ucsd.edu>
2014-10-15 12:07:11 -0700

    Mailing scripts included

---
+++ runAllMail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo beginning script!
+./seatmeW3
+./seatmeW7
+./seatmeTh9
+./seatmeTh11
+./seatmeTh1
+./seatmeTh3
+./seatmeTh5
+echo succesfulCompletion
---
+++ toTxt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /Development/15lseating.bak
+echo beginning script!
+cp roster.Tu9 roster.Tu9.txt
+cp roster.Tu3 roster.Tu3.txt
+cp roster.Tu5 roster.Tu5.txt
+cp roster.W9 roster.W9.txt
+cp roster.W3 roster.W3.txt
+cp roster.Th9 roster.Th9.txt
+cp roster.Th3 roster.Th3.txt
+cp roster.Fr3 roster.Fr3.txt
+echo succesfulCompletion
+
